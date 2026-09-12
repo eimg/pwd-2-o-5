611b5158a62512b77655ce8d447a55323878a9f8
Ei Maung <eimg@fairwayweb.com>
2026-09-12 08:25:42 +0630

    add post

--- social-api/routes/posts.ts
+++ social-api/routes/posts.ts
@@ -3,6 +3,8 @@ export const router = express.Router();
 
 import { prisma } from "../lib/prisma";
 
+import { auth } from "../middlewares/auth";
+
 router.get("/posts", async (req, res) => {
 	const posts = await prisma.post.findMany({
 		orderBy: {
@@ -16,6 +18,23 @@ router.get("/posts", async (req, res) => {
 	});
 
 	res.json(posts);
+});
+
+router.post("/posts", auth, async (req, res) => {
+	const content = req.body?.content;
+	if (!content) {
+		return res.status(400).json({ msg: "content is required" });
+	}
+
+	const userId = res.locals.user.id as number;
+	const post = await prisma.post.create({
+		data: {
+			content,
+			userId,
+		},
+	});
+
+	res.status(201).json(post);
 });
 
 router.get("/posts/:id", async (req, res) => {
--- social-app/src/pages/Home.jsx
+++ social-app/src/pages/Home.jsx
@@ -1,8 +1,12 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Button, OutlinedInput, Typography } from "@mui/material";
 
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 
 import PostCard from "../components/PostCard";
+
+import { useRef } from "react";
+
+import { useApp } from "../AppProvider";
 
 async function fetchPosts() {
 	const res = await fetch("http://localhost:8800/posts");
@@ -10,6 +14,12 @@ async function fetchPosts() {
 }
 
 export default function Home() {
+	const contentRef = useRef();
+
+	const { auth } = useApp();
+
+	const queryClient = useQueryClient();
+
 	const {
 		data: posts,
 		isLoading,
@@ -35,10 +45,62 @@ export default function Home() {
 		);
 	}
 
+	async function submitPost(content) {
+		const token = localStorage.getItem("token");
+
+		const res = await fetch("http://localhost:8800/posts", {
+			method: "POST",
+			body: JSON.stringify({ content }),
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${token}`,
+			},
+		});
+
+		if (res.ok) {
+			await queryClient.invalidateQueries({ queryKey: ["posts"] });
+		} else {
+			alert("Unable to submit post");
+		}
+	}
+
 	return (
 		<Box>
+			{auth && (
+				<Box sx={{ mb: 2, textAlign: "right" }}>
+					<form
+						onSubmit={e => {
+							e.preventDefault();
+							const content = contentRef.current.value;
+
+							if (content) {
+								submitPost(content);
+							}
+
+							e.currentTarget.reset();
+						}}>
+						<OutlinedInput
+							fullWidth
+							placeholder="What's on your mind?"
+							inputRef={contentRef}
+						/>
+						<Button
+							type="submit"
+							sx={{ mt: 1 }}
+							variant="contained">
+							Add Post
+						</Button>
+					</form>
+				</Box>
+			)}
+
 			{posts?.map(post => {
-				return <PostCard key={post.id} post={post} />;
+				return (
+					<PostCard
+						key={post.id}
+						post={post}
+					/>
+				);
 			})}
 		</Box>
 	);
